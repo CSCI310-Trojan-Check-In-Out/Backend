8e014fecf5bf226bfe74908f4cc151e6f15c12db
TerryT <terrytzh0607@gmail.com>
2021-03-12 14:37:48 -0800

    Add logout and changePassword route

--- routers/account.js
+++ routers/account.js
@@ -1,6 +1,5 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
 
 router.get('/', function(req, res) {
     res.send("Account endpoint page. This is used to serve all APIs related to account management (registration, log in, etc.).");
@@ -18,11 +17,11 @@ router.post('/register', function(req, res) {
         return;
     }
 
-    let fullName = req.body.fullname;
-    let uscId = req.body.uscid;
+    let fullName = req.body.fullName;
+    let uscId = req.body.uscId;
     let password = req.body.password;
-    let uscEmail = req.body.uscemail;
-    let accountType = req.body.accounttype;
+    let uscEmail = req.body.uscEmail;
+    let accountType = req.body.accountType;
 
     // Firebase is going to handle profile picture upload
     // let profilePic = req.body.profilepic;
@@ -33,9 +32,10 @@ router.post('/register', function(req, res) {
         uscEmail === undefined ||
         accountType === undefined) {
         res.status(400).send("Missing form data.");
+        return;
     }
 
-    // Stub: Check if same uscId already exists in database and store it if needed
+    // TODO: Check if same uscId already exists in database and store it if needed
 
     // Temporary field; this should be guaranteed to be its userid
     req.session.userid = uscId;
@@ -62,11 +62,37 @@ router.post('/login', function (req, res) {
         return;
     }
 
-    // Stub: Validate user credentials in the database
+    // TODO: Validate user credentials in the database
 
     // Temporary field; this should be guaranteed to be its userid
     req.session.userid = username;
     res.sendStatus(200);
 });
+
+router.post('/logout', function(req, res) {
+    req.session.destroy();
+    res.sendStatus(200);
+});
+
+router.post('/changePassword', function(req, res) {
+    if(req.header('Content-Type') !== 'application/x-www-form-urlencoded') {
+        res.status(400).send("Wrong form Content-Type. Should be application/x-www-form-urlencoded.");
+        return;
+    }
+
+    if(!req.session.userid) {
+        res.status(400).send("The client is not logged in.");
+        return;
+    }
+
+    let oldPassword = req.body.oldPassword;
+    let newPassword = req.body.newPassword;
+
+    // TODO: Validate user credentials with session.userid and oldPassword, update to newPassword if valid
+
+    req.sendStatus(200);
+});
+
+
 
 module.exports = router;
