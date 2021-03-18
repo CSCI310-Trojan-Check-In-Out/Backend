1641f9416674bbd7fed688743a175c737e8703d5
Runmu <runmuyue@usc.edu>
2021-03-18 22:01:49 +0800

    crash

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || config.URI
+    connectionString: config.URI
 });
 
 module.exports = {pool};
