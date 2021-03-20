55e903537c6ac7c0670128f7b8ff5fd9ad91bf02
TerryT <terrytzh0607@gmail.com>
2021-03-19 22:36:09 -0700

    Clean unnecessary codes

--- routers/account.js
+++ routers/account.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const multer = require('multer');
 const Router = require('express-promise-router')
 const upload = multer();
--- routers/student.js
+++ routers/student.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const Router = require('express-promise-router')
+const router = Router();
 
 router.get('/', function(req, res) {
     res.send("Student endpoint page. This is used to serve all APIs related to student clients (check in / out, view history).");
