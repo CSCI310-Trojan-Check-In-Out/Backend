1dc297043ab4b67cfaa7b21d47bbaf41b92cbb76
TerryT <terrytzh0607@gmail.com>
2021-03-21 13:46:04 -0700

    Further improve database structure

--- database/database.sql
+++ database/database.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS account CASCADE;
 
 CREATE TABLE account (
 	id SERIAL UNIQUE PRIMARY KEY CHECK (id > 0),
-    usc_id VARCHAR(20),
-	username VARCHAR(100) UNIQUE,
+    usc_id VARCHAR(20) UNIQUE,
+	username VARCHAR(100),
 	major VARCHAR(100),
     email VARCHAR(100) UNIQUE,
     passcode VARCHAR(64),
@@ -14,11 +14,11 @@ CREATE TABLE account (
 );
 
 CREATE TABLE place (
-	id SERIAL PRIMARY KEY CHECK (id > 0),
+	id SERIAL UNIQUE PRIMARY KEY CHECK (id > 0),
 	place_name VARCHAR(100) UNIQUE,
     abbreviation VARCHAR(10) UNIQUE,
     place_address VARCHAR(200),
-    qr_code_token VARCHAR(100),
+    qr_code_token uuid DEFAULT gen_random_uuid(),
     picture VARCHAR(2083),
     capacity INT,
     current_numbers INT,
@@ -27,7 +27,7 @@ CREATE TABLE place (
 );
 
 CREATE TABLE visit_history (
-	id SERIAL PRIMARY KEY CHECK (id > 0),
+	id SERIAL UNIQUE PRIMARY KEY CHECK (id > 0),
 	account_id INT REFERENCES account(id) ON DELETE CASCADE,
     place_id INT REFERENCES place(id) ON DELETE CASCADE,
     enter_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
