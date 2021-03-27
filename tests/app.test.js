7fc600396214390dc5be23edc2b35fb82e294ac4
TerryT <terrytzh0607@gmail.com>
2021-03-27 03:08:28 -0700

    Fix jest open handles

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,18 +1,38 @@
 const request = require("supertest");
+const http = require("http");
 const app = require("../app");
 
 describe("Test the root path", () => {
+    let server;
+    beforeAll((done) => {
+        server = http.createServer(app);
+        server.listen(done);
+    });
+
+    afterAll((done) => {
+        server.close(done);
+    });
+
     test("It should respond with 404", async () => {
-        const response = await request(app).get("/");
+        const response = await request(server).get("/");
         expect(response.statusCode).toBe(404);
         expect(response.text).toBe("Resource not found");
     });
 });
 
 describe("Test the login route", () => {
+    let server;
+    beforeAll((done) => {
+        server = http.createServer(app);
+        server.listen(done);
+    });
+
+    afterAll((done) => {
+        server.close(done);
+    });
+
     test("It should respond with 404", async () => {
-        process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-        const response = await request(app).post("/account/login")
+        const response = await request(server).post("/account/login")
             .field("email", "null")
             .field("password", "null");
         expect(response.statusCode).toBe(200);
