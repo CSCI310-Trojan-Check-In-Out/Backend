976fe2574021944014e0a1d6ad9f1507bdc5a55e
Shihao Huang <huan773@usc.edu>
2021-04-03 21:05:37 -0700

    update firebase sync function

--- firebase/firebaseSync.js
+++ firebase/firebaseSync.js
@@ -3,7 +3,7 @@
 /* -------------------------------------------------------------------------- */
 
 var admin = require("firebase-admin");
-const {pool: pool} = require('../database/db');
+const { pool: pool } = require("../database/db");
 var serviceAccount = require("../firebaseServiceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,25 +14,39 @@ const realTimeDb = admin.database();
 /*                                   methods                                  */
 /* -------------------------------------------------------------------------- */
 
-const userCheckin = (buildingId, userId) => {
+/*
+userInfo = {
+  id: "1223",
+  usc_id: "123456788",
+  first_name: "Nate",
+  last_name: "Huang",
+  full_name: "Nate Huang",
+  major: "computer",
+  email: "huan773@usc.edu",
+  picture: "google.com/image",
+  is_admin: 1,
+  is_deleted: 0,
+};
+*/
+const userCheckin = (buildingId, userId, userInfo) => {
   const ref = realTimeDb.ref(`buildings/${buildingId}/checkin`);
-  ref.child("user"+userId).set("true");
+  ref.child("user" + userId).set(userInfo);
 };
 
 const userCheckout = (buildingId, userId) => {
   const ref = realTimeDb.ref(`buildings/${buildingId}/checkin`);
-  ref.child("user"+userId).remove();
+  ref.child("user" + userId).remove();
 };
 
 const updateMaximumCapacity = (buildingId, maximumCapacity) => {
-    const ref = realTimeDb.ref(`buildings/${buildingId}/capacity`);
-    ref.set(maximumCapacity);
-}
+  const ref = realTimeDb.ref(`buildings/${buildingId}/capacity`);
+  ref.set(maximumCapacity);
+};
 
 const deleteAll = () => {
   const ref = realTimeDb.ref(`buildings`);
   ref.remove();
-}
+};
 
 const syncAllLocations = async () => {
   // console.log("syncAllLocations in firebase.");
@@ -41,12 +55,12 @@ const syncAllLocations = async () => {
     .then(async (res) => {
       res.rows.forEach((location) => {
         updateMaximumCapacity(location.id, location.capacity);
-      })
+      });
     })
     .catch((err) => {
       console.log(err);
     });
-}
+};
 
 const syncAllCheckins = async () => {
   await pool
@@ -54,14 +68,21 @@ const syncAllCheckins = async () => {
     .then(async (res) => {
       res.rows.forEach((record) => {
         userCheckin(record.place_id, record.account_id);
-      })
+      });
     })
     .catch((err) => {
       console.log(err);
     });
-}
+};
 
-module.exports = { userCheckin, userCheckout, updateMaximumCapacity, deleteAll, syncAllLocations, syncAllCheckins };
+module.exports = {
+  userCheckin,
+  userCheckout,
+  updateMaximumCapacity,
+  deleteAll,
+  syncAllLocations,
+  syncAllCheckins,
+};
 
 /* -------------------------------------------------------------------------- */
 /*                                    usage                                   */
